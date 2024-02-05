6be32329cbb6740af60f76d22f00dec986e263c2
Crowdbotics <team@crowdbotics.com>
2024-02-05 20:12:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "parallelapp-edge-45979",
+  url: "https://parallelapp-edge-45979.botics.co",
+  api: "https://parallelapp-edge-45979.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
